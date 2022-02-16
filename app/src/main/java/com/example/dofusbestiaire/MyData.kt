e6badbbb4d696632224fca483f3817cb9866f8d6
Tom Wallyn <tom.wallyn@leroymerlin.fr>
2022-02-16 14:12:51 +0100

    feat(api): add api data and interface

---
+++ app/src/main/java/com/example/dofusbestiaire/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.dofusbestiaire
+
+class MyData : ArrayList<MyDataItem>()
---
+++ app/src/main/java/com/example/dofusbestiaire/MyDataItem.kt
@@ -0,0 +1,8 @@
+package com.example.dofusbestiaire
+
+data class MyDataItem(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
