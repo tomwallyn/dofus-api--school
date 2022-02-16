f543d54ff1fa41d51f1b12967261721c0198c428
RomainP01 <romainpanii@gmail.com>
2022-02-16 18:00:32 +0100

    test initiation glide

--- app/src/main/java/com/example/dofusbestiaire/MainActivity.kt
+++ app/src/main/java/com/example/dofusbestiaire/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.dofusbestiaire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<ImageView>(R.id.testImage)
+        Glide.with(view).load("https://s.ankama.com/www/static.ankama.com/dofus/www/game/items/200/18006.png").into(view)
     }
+
 }
