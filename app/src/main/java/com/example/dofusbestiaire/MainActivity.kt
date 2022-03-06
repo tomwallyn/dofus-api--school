66fafa8c4720807151268e2ea6c601463a8561c6
RomainP01 <romainpanii@gmail.com>
2022-03-06 23:47:05 +0100

    ebauche d'une premiere version : mise en place du layout principale, compréhension du xml

--- app/src/main/java/com/example/dofusbestiaire/MainActivity.kt
+++ app/src/main/java/com/example/dofusbestiaire/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.dofusbestiaire
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val view = findViewById<ImageView>(R.id.testImage)
-        Glide.with(view).load("https://s.ankama.com/www/static.ankama.com/dofus/www/game/items/200/18006.png").into(view)
+        setContentView(R.layout.homepage)
+        val imageView = findViewById<ImageView>(R.id.cardImage)
+        Glide.with(imageView).load("https://s.ankama.com/www/static.ankama.com/dofus/www/game/items/200/18006.png").into(imageView)
+        val textView = findViewById<TextView>(R.id.cardText)
+        
     }
 
 }
