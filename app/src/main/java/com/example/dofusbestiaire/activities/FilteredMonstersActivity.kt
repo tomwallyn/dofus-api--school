0352f3e88b35618ca2684ba4700d12302e30937c
RomainP01 <romainpanii@gmail.com>
2022-03-30 11:19:40 +0200

    fixing search bar

--- app/src/main/java/com/example/dofusbestiaire/activities/FilteredMonstersActivity.kt
+++ app/src/main/java/com/example/dofusbestiaire/activities/FilteredMonstersActivity.kt
@@ -1,4 +1,92 @@
 package com.example.dofusbestiaire.activities
 
-class FilteredMonstersActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dofusbestiaire.R
+import com.example.dofusbestiaire.components.ListViewAdapter
+import com.example.dofusbestiaire.data.ApiClient
+import com.example.dofusbestiaire.models.Monsters
+import com.example.dofusbestiaire.ui.RecyclerViewCardCreator
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class FilteredMonstersActivity : AppCompatActivity(){
+    lateinit var recyclerViewCardCreator: RecyclerViewCardCreator
+    lateinit var adapter: ListViewAdapter
+    lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.filtered_monsters_layout)
+        val allMonsters = callApi()
+        recycler(allMonsters)
+        adapter = ListViewAdapter(this, allMonsters, recyclerViewCardCreator)
+        bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation)
+        this.configureBottomView()
+    }
+
+    fun recycler(allMonsters:List<Monsters>){
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewMonster)
+        recyclerViewCardCreator= RecyclerViewCardCreator(allMonsters,this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = recyclerViewCardCreator
+    }
+    fun callApi(): List<Monsters> {
+        var content = listOf<Monsters>()
+        runBlocking {
+            launch {
+                try {
+                    val response = ApiClient.apiService.getMonsters()
+
+                    if (response.isSuccessful && response.body() != null) {
+                        content = response.body()!!
+//do something
+                    } else {
+                        Toast.makeText(
+                            this@FilteredMonstersActivity,
+                            "Error Occurred: ${response.message()}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        this@FilteredMonstersActivity,
+                        "Error Occurred: ${e.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+            return@runBlocking content
+        }
+        return content
+    }
+
+    fun configureBottomView() {
+        bottomNavigationView.setOnItemReselectedListener { item -> updateMainFragment(item.itemId) }
+    }
+
+    fun updateMainFragment(integer: Int): Boolean {
+        when (integer) {
+            R.id.home -> {
+                val intent = Intent(this, AllMonstersActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            }
+            R.id.filters ->{
+                val intent = Intent(this, FilteredMonstersActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            }
+
+
+        }
+        return true
+    }
+
 }
